920b69c17342dedf1f751336bf3a51df831f146e
Alan Li <alan.li@me.com>
2016-07-28 17:12:30 -0400

    adding LLVM.General.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified LLVM.General.AST as AST
+import LLVM.General.Module
+
+stringAsASTModule :: BS.ByteString -> AST.Module
+stringAsASTModule = read
+
+ast2module :: AST.Module -> IO Module
+ast2module mod = withContext $ \context -> liftError $ withModuleFromAST context mod
+
+
