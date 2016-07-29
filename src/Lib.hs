a8d815527415de170c0caedfd55ff492daf7240c
Alan Li <alan.li@me.com>
2016-07-28 21:16:47 -0400

    Revert "adding LLVM.General."
    This reverts commit 920b69c17342dedf1f751336bf3a51df831f146e.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,6 @@
-module Lib where
+module Lib
+    ( someFunc
+    ) where
 
-import qualified LLVM.General.AST as AST
-import LLVM.General.Module
-
-stringAsASTModule :: BS.ByteString -> AST.Module
-stringAsASTModule = read
-
-ast2module :: AST.Module -> IO Module
-ast2module mod = withContext $ \context -> liftError $ withModuleFromAST context mod
-
-
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
